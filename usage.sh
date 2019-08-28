091f84c91c5fda46e7bbf3b81f4b5ff98f7a81e8
Robin van der Noord <robinvandernoord@gmail.com>
2019-08-28 21:24:26 +0200

    de drivers van medion zijn echt super guur dus ik moest ze handmatig aanpassen

---
+++ usage.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#https://github.com/lwfinger/rtl8723bu
+apt install dkms -y
+source dkms.conf
+sudo mkdir /usr/src/$PACKAGE_NAME-$PACKAGE_VERSION
+sudo cp -r core hal include os_dep platform dkms.conf Makefile rtl8723b_fw.bin /usr/src/$PACKAGE_NAME-$PACKAGE_VERSION
+sudo dkms add -m $PACKAGE_NAME/$PACKAGE_VERSION # anders dan op github staat!
+sudo dkms autoinstall $PACKAGE_NAME/$PACKAGE_VERSION
+sudo echo 'blacklist rtl8xxxu' >> /etc/modprobe.d/blacklist.conf  # staat ook niet op github, wel nodig anders laadt hij oude driver
+
